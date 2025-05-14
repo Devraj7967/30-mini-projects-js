8f7ead5f817d53feb2d7b6978ec331ba59670486
DEVRAJ PARMAR <140972280+Devraj7967@users.noreply.github.com>
2025-05-14 23:42:14 +0530

    Delete Day 4 directory

--- Day 4/main.js
+++
@@ -1,8 +0,0 @@
-const toggleBtn = document.querySelector(".toggle-btn");
-const toggleContent = document.querySelector(".toggle-content");
-
-toggleBtn.addEventListener("click", () => {
-  toggleContent.style.display = toggleContent.style.display === "none" || toggleContent.style.display === ""
-    ? "block"
-    : "none";
-});
