1f0ea73c199e74fc97e51c72ce8a501600d853f1
devraj <devparmar6344@gmail.com>
2025-06-04 10:45:29 +0530

    mini projects

---
+++ counter app/main.js
@@ -0,0 +1,21 @@
+const count = document.querySelector('.count')
+const minusBtn = document.querySelector('.minus-btn')
+const plusbtn = document.querySelector('.plus-btn')
+const changeby = document.querySelector('.changeby')
+const reset = document.querySelector('.reset')
+
+minusBtn.addEventListener('click', () => {
+    const countValue = parseInt(count.innerText);
+    const changevalue = parseInt(changeby.value)
+    count.innerText = countValue - 1;
+})
+
+plusbtn.addEventListener('click', () => {
+    const countValue = parseInt(count.innerText)
+    const changevalue = parseInt(changeby.value)
+    count.innerText = countValue + changevalue
+})
+
+reset.addEventListener('click', () => {
+    count.innerText = 0
+})
